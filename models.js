9590889df4026ecd18ccc6d4f30d46f7e7a0e268
McRae Petrey <mpetrey@gmail.com>
2018-11-12 17:03:43 -0500

    sending over to Jing - McRae is getting errors

---
+++ models.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/plantr', {logging: false});
+
+const Gardener = db.define('gardeners', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+const Plot = db.define('plots' , {
+  size: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+
+})
+
+const Vegetable = db.define('vegetables', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  planted_on: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
+})
+
+Plot.belongsTo(Gardener, {as: 'plotOwner'});
+
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'})
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
+
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
+
+
+module.exports = { db , Gardener, Plot, Vegetable};
